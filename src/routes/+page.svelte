e4521cd0021f375c37bd8896823fea3d36ed812c
baheit <arturbaheit@gmail.com>
2023-06-11 14:03:17 +0300

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+  let hhmmss = "";
+
+  setInterval(() => {
+    hhmmss = new Date().toLocaleTimeString();
+  }, 1000);
+	let color='pink';
+	let name='Посетитель сайта';
+</script>
+
+<lable for='name'>Имя</lable>
+<input id='name' bind:value={name}>
+<label for='color'>Цвет</label>
+<input id='color' type='color' bind:value={color}>
+<div style='background-color: {color}' class='swatch'/>
+<h1 style='color: {color}'>Привет {name}!</h1>
+
+<div>
+	 I will be a clock.
+</div>
+<span style='color: {color}'>{hhmmss}</span>
+
+<style>
+	.swatch {
+		display: inline-block;
+		height: 20px;
+		width: 20px;
+	}
+</style>
